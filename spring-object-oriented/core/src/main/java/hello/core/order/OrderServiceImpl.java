296554e1ecd87cf13c0304f9f52ad2f80ae41b08
MoonSueLym <msr56501354@gmail.com>
2021-02-11 16:17:16 +0900

    할인 정책 변경 -> DIP,OCP위반

--- spring-object-oriented/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring-object-oriented/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,11 +5,16 @@ import hello.core.Member.MemberRepository;
 import hello.core.Member.MemoryMemberRepository;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    // DIP위반 : OrderServiceImpl이라는 객체가 DiscountPolicy는 RateDiscountPolicy를 써야한다고 지정
+    // 레오나르도 디카프리오가 여주인공을 고르는 셈
+
     @Override
     public Order createOrder(Long memberId, int itemPrice, String itemName) {
         Member member = memberRepository.findById(memberId);
