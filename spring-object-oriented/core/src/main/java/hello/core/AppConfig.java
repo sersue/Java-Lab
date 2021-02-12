795a997a53137f24a86dfe0b6e962823b3474729
MoonSueLym <msr56501354@gmail.com>
2021-02-12 20:29:48 +0900

    AppConfig 리팩토링 (역할 잘보이게)

--- spring-object-oriented/core/src/main/java/hello/core/AppConfig.java
+++ spring-object-oriented/core/src/main/java/hello/core/AppConfig.java
@@ -9,9 +9,13 @@ import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
         /*
@@ -21,15 +25,15 @@ public class AppConfig {
           DI(Dependency Injection)이라고 함
         */
     }
-
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
