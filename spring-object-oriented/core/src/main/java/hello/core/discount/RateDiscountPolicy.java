ef1d313c4f9d6a903ecf36a725ab57d86a00693f
MoonSueLym <msr56501354@gmail.com>
2021-02-15 16:09:17 +0900

    annotation 직접 만들기

--- spring-object-oriented/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ spring-object-oriented/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,9 +2,13 @@ package hello.core.discount;
 
 import hello.core.Member.Grade;
 import hello.core.Member.Member;
+import hello.core.annotation.MainDiscountPolicy;
 import org.springframework.stereotype.Component;
 
+
+
 @Component
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent =10;
--- spring-object-oriented/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring-object-oriented/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,13 +3,16 @@ package hello.core.order;
 import hello.core.Member.Member;
 import hello.core.Member.MemberRepository;
 import hello.core.Member.MemoryMemberRepository;
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -23,7 +26,7 @@ public class OrderServiceImpl implements OrderService{
     private final DiscountPolicy discountPolicy;
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
